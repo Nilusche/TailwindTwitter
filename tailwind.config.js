5657587ec3f0eb9ce9bf1ffa7020ac2d6bcdbac3
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-03-02 12:30:24 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {colors:{
+      twblue:{
+        100: "#1DA1F2",
+        200: "#0d8bd9",
+      },
+      twdarkblue:"#0c81ca",
+      twgrey:{
+        100: "#F5F8FA",
+        200: "#E1E8ED",
+        300: "#AAB8C2",
+        400: "#657786"
+      },
+      twblue:"#1DA1F2",
+      twgreen:"#42E6A4",
+      twred:"#E60965"
+    },
+      fontFamily:{
+        'montserrat':['Roboto', 'sans-serif']
+      }
+      
+    }
+  },
+  plugins: [],
+}
